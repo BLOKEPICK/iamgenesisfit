ecad9c5a9939e3d321103faeaf32b9f27124dd8f
BLOKEPICK <Blokeplaycom@gmail.com>
2025-06-28 19:18:25 -0400

    UPDATE

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", fontFamily: "sans-serif" }}>
+      <h1>Bienvenido a iamgenesisfit</h1>
+    </main>
+  );
+}
