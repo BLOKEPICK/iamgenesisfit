d2486891475908b3d25ac513b237a71e6041d04a
BLOKEPICK <Blokeplaycom@gmail.com>
2025-07-24 04:14:18 -0400

    U

--- pages/Preview/index.js
+++ pages/Preview/index.js
@@ -87,6 +87,7 @@ export default function Home() {
               gap: 1rem;
               width: 100%;
               padding-bottom: 2rem;
+              margin-top: 2rem;
             }
           }
           .hero-button {
